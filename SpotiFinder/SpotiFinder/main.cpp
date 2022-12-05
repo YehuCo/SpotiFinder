0c4ad5702961038804fc1f66a8db56e17582b7dc
Yehuda <yehuda.c003@gmail.com>
2022-12-04 22:12:22 -0500

    new csv file

--- SpotiFinder/SpotiFinder/main.cpp
+++ SpotiFinder/SpotiFinder/main.cpp
@@ -1,31 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 #include "MergeSort.h"
 
 using namespace std;
 
 int main() {
-	string word;
 
-	// print welcome screen
-	cout << "Wlecome to SpotiFinder." << endl;
-	cout << "Please enter the word you'd like to use to find song titles: ";
-	cin >> word;
+	fstream myFile;
+	myFile.open("SpotiFinderSubset.csv");
+	string line;
+	getline(myFile, line, '\n');
+	cout << line << endl;
+	for (int i = 0; i < 2; i++) {
+		getline(myFile, line, '\n');
+		cout << line << endl;
+	}
 
-	/*
-	TODO: call function in the hash table class to access index where word is stored and retrieve the stored vector of titles
-	*/
 
-	/*
-	TODO: call function in the graph class to retrieve list of song titles with inputed word
-	*/
-	
-	cout << "Songs containing the word " << word << " stored in the hash table:" << endl;
-	// print list of songs from the hash table
 
-	cout << "Songs containing the word " << word << " stored in the graph:" << endl;
-	// print list of songs from the graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//string word;
+
+	//// print welcome screen
+	//cout << "Wlecome to SpotiFinder." << endl;
+	//cout << "Please enter the word you'd like to use to find song titles: ";
+	//cin >> word;
+
+	///*
+	//TODO: call function in the hash table class to access index where word is stored and retrieve the stored vector of titles
+	//*/
+
+	///*
+	//TODO: call function in the graph class to retrieve list of song titles with inputed word
+	//*/
+	//
+	//cout << "Songs containing the word " << word << " stored in the hash table:" << endl;
+	//// print list of songs from the hash table
+
+	//cout << "Songs containing the word " << word << " stored in the graph:" << endl;
+	//// print list of songs from the graph
 
 
 	return 0;
