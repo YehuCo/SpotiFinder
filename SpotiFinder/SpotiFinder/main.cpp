b648fbd7d02809790e0a1b2485e2b38bfb94f0a3
Yehuda <yehuda.c003@gmail.com>
2022-12-04 20:53:10 -0500

    merge sort:

--- SpotiFinder/SpotiFinder/main.cpp
+++ SpotiFinder/SpotiFinder/main.cpp
@@ -9,7 +9,7 @@ int main() {
 	string word;
 
 	// print welcome screen
-	cout << "Welcome to SpotiFinder." << endl;
+	cout << "Wlecome to SpotiFinder." << endl;
 	cout << "Please enter the word you'd like to use to find song titles: ";
 	cin >> word;
 
@@ -27,16 +27,6 @@ int main() {
 	cout << "Songs containing the word " << word << " stored in the graph:" << endl;
 	// print list of songs from the graph
 
-	Sorter vec;
-	vec.list.push_back(Sorter::Song("yehuda", 0.5));
-	vec.list.push_back(Sorter::Song("emma", 0.8));
-	vec.list.push_back(Sorter::Song("seany", 0.3));
-	vec.list.push_back(Sorter::Song("mario", 0.2));
-	vec.mergeSort(vec.list, 0, vec.list.size() - 1);
-
-	for (int i = 0; i < vec.list.size(); i++) {
-		cout << vec.list[i].title << " " << vec.list[i].hotness << endl;
-	}
 
 	return 0;
 }
