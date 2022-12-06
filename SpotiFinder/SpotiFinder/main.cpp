f2185a6c00a5d808a0734ac75056f80d542044c3
Yehuda <yehuda.c003@gmail.com>
2022-12-06 13:33:39 -0500

    slightly more organized main

--- SpotiFinder/SpotiFinder/main.cpp
+++ SpotiFinder/SpotiFinder/main.cpp
@@ -1,39 +1,57 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 #include "Sorters.h"
-
-//#include "TempSorters.h"
 using namespace std;
+
 
 int main() {
 	Containers songs;
 	songs.populateData();
-	string word;
-
-
-	cin >> word;
-
-
-	// print welcome screen
-	//cout << "SpotiFinder." << endl;
-	//cout << "Please enter the word you'd like to use to find song titles: ";
-	//cin >> word;
-
-	/*
-	TODO: call function in the hash table class to access index where word is stored and retrieve the stored vector of titles
-	*/
-
-	/*
-	TODO: call function in the graph class to retrieve list of song titles with inputed word
-	*/
+	string searchWord;
+	string sortMethod;
+	string restart;
 	
-	//cout << "Songs containing the word " << word << " stored in the hash table:" << endl;
-	// print list of songs from the hash table
 
-	//cout << "Songs containing the word " << word << " stored in the graph:" << endl;
-	// print list of songs from the graph
+	// print welcome screen and prompt for search word
+	cout << "--------------------SpotiFinder--------------------\n" << endl;
+	while (true) {
+		cout << "Please enter the word you'd like to use to find song titles: ";
+		cin >> searchWord;
 
+		// ask user if they'd like to sort using MergeSort or QuickSort
+		cout << "How would you like the list of songs to be sorted? (quick/merge) ";
+		cin >> sortMethod;
+
+		auto start = chrono::system_clock::now();
+		// if input is "Quick" call quick sort on the list of songs
+		if (sortMethod == "quick") {
+			QuickSort(songs.searchMap[searchWord]);
+		}
+		// call merge sort if the input is "Merge"
+		else if (sortMethod == "merge") {
+			MergeSort(songs.searchMap[searchWord], 0, songs.searchMap[searchWord].size() - 1);
+		}
+		auto end = chrono::system_clock::now();
+		chrono::duration<double> elapsed_seconds = end - start;
+
+		// print the list of songs to the terminal
+		cout << "Song list:" << endl;
+		for (auto song : songs.searchMap[searchWord]) {
+			cout << "Song name: " << song.name << ". Popularity: " << song.hotness << endl;
+		}
+
+		// print the time it took to perform the sort
+		cout << "Elapsed time to perform sort: " << elapsed_seconds.count() << "s" << endl;
+		
+		// ask if user wants to perform another search
+		cout << "Would you like to perform another search? (y/n) ";
+		cin >> restart;
+		if (restart != "y") {
+			break;
+		}
+	}
 
 	return 0;
 }
