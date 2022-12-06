2f1cc288d9fc85c7cbc81739df1b5caa9ba7b50b
Yehuda <yehuda.c003@gmail.com>
2022-12-05 22:17:10 -0500

    implemented hash table, reorganized class

--- SpotiFinder/SpotiFinder/main.cpp
+++ SpotiFinder/SpotiFinder/main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Sorter.h"
+#include "Sorters.h"
+
+//#include "TempSorters.h"
 using namespace std;
 
 int main() {
-
-	Sorter songs;
-
+	Containers songs;
 	songs.populateData();
-	songs.mergeSort(0, songs.songList.size() - 1);
-
-
-	
 	string word;
 
-	// print welcome screen
-	cout << "SpotiFinder." << endl;
-	cout << "Please enter the word you'd like to use to find song titles: ";
+
 	cin >> word;
+
+
+	// print welcome screen
+	//cout << "SpotiFinder." << endl;
+	//cout << "Please enter the word you'd like to use to find song titles: ";
+	//cin >> word;
 
 	/*
 	TODO: call function in the hash table class to access index where word is stored and retrieve the stored vector of titles
@@ -28,10 +28,10 @@ int main() {
 	TODO: call function in the graph class to retrieve list of song titles with inputed word
 	*/
 	
-	cout << "Songs containing the word " << word << " stored in the hash table:" << endl;
+	//cout << "Songs containing the word " << word << " stored in the hash table:" << endl;
 	// print list of songs from the hash table
 
-	cout << "Songs containing the word " << word << " stored in the graph:" << endl;
+	//cout << "Songs containing the word " << word << " stored in the graph:" << endl;
 	// print list of songs from the graph
 
 
